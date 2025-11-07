7170f67390437cc1cf70b3abb3e5a8c3ec3cd230
nyrta1 <nurtai_2005@mail.ru>
2025-11-07 15:14:28 +0500

    fix: add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 // src/app.js
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.get('/ping', (req, res) => {
     res.send(`Hello world`);
 });
